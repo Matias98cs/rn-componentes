9921e5359bfeeb17a7ad9b4921e4743c66480ac7
Matias Campos <matias98cs@gmail.com>
2024-11-18 18:07:19 -0300

    Slides - configuracion y personalizacion

--- app/slides/index.tsx
+++ app/slides/index.tsx
@@ -1,10 +1,143 @@
-import { View, Text } from 'react-native';
+import ThemedButton from "@/presentation/shared/ThemedButton";
+import ThemedText from "@/presentation/shared/ThemedText";
+import ThemedView from "@/presentation/shared/ThemedView";
+import { router, useNavigation } from "expo-router";
+import { useLayoutEffect, useRef, useState } from "react";
+import {
+  View,
+  Text,
+  ImageSourcePropType,
+  FlatList,
+  Image,
+  useWindowDimensions,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+} from "react-native";
+
+interface Slide {
+  title: string;
+  desc: string;
+  img: ImageSourcePropType;
+}
+
+const items: Slide[] = [
+  {
+    title: "Titulo 1",
+    desc: "Ea et eu enim fugiat sunt reprehenderit sunt aute quis tempor ipsum cupidatat et.",
+    img: require("../../assets/images/slide-1.png"),
+  },
+  {
+    title: "Titulo 2",
+    desc: "Anim est quis elit proident magna quis cupidatat curlpa labore Lorem ea. Exercitation mollit velit in aliquip tempor occaecat dolor minim amet dolor enim cillum excepteur. ",
+    img: require("../../assets/images/slide-2.png"),
+  },
+  {
+    title: "Titulo 3",
+    desc: "Ex amet duis amet nulla. Aliquip ea Lorem ea culpa consequat proident. Nulla tempor esse ad tempor sit amet Lorem. Velit ea labore aute pariatur commodo duis veniam enim.",
+    img: require("../../assets/images/slide-3.png"),
+  },
+];
 
 const SlidesScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [navigation]);
+  const flatListRef = useRef<FlatList>(null);
+  const [currentSliceIndex, setCurrentSliceIndex] = useState(0);
+
+  const [isScroll, setIsScroll] = useState(false);
+
+  const onScroll = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if (isScroll) {
+      return;
+    }
+
+    const { contentOffset, layoutMeasurement } = e.nativeEvent;
+    const currentIndex = Math.floor(contentOffset.x / layoutMeasurement.width);
+    setCurrentSliceIndex(currentIndex > 0 ? currentIndex : 0);
+
+    if (currentIndex === items.length - 1) {
+      setIsScroll(true);
+    }
+  };
+
+  const scrollToSlice = (index: number) => {
+    if (!flatListRef.current) return;
+
+    flatListRef.current.scrollToIndex({
+      index: index,
+      animated: true,
+    });
+  };
+
   return (
-    <View>
-      <Text>SlidesScreen</Text>
-    </View>
+    <ThemedView>
+      <FlatList
+        ref={flatListRef}
+        data={items}
+        keyExtractor={(item) => item.title}
+        renderItem={({ item }) => <SliceItem item={item} />}
+        horizontal
+        pagingEnabled
+        scrollEnabled={isScroll}
+        onScroll={onScroll}
+      />
+
+      {currentSliceIndex === items.length - 1 ? (
+        <ThemedButton
+          className="absolute bottom-10 right-5 w-[150px]"
+          onPress={() => router.dismiss()}
+        >
+          Finalizar
+        </ThemedButton>
+      ) : (
+        <ThemedButton
+          onPress={() => scrollToSlice(currentSliceIndex + 1)}
+          className="absolute bottom-10 right-5 w-[150px]"
+        >
+          Siguiente
+        </ThemedButton>
+      )}
+    </ThemedView>
   );
 };
 export default SlidesScreen;
+
+interface SlideItemProps {
+  item: Slide;
+}
+
+const SliceItem = ({ item }: SlideItemProps) => {
+  const { width } = useWindowDimensions();
+  const { title, desc, img } = item;
+
+  return (
+    <ThemedView
+      style={{
+        width: width,
+      }}
+      className="flex-1 rounded p-10 justify-center bg-red-400"
+    >
+      <Image
+        source={img}
+        style={{
+          width: width * 0.7,
+          height: width * 0.7,
+          resizeMode: "center",
+          alignSelf: "center",
+        }}
+      />
+      <ThemedText
+        type="h1"
+        className="text-ligth-primary dark:text-dark-primary"
+      >
+        {title}
+      </ThemedText>
+
+      <ThemedText className="mt-10">{desc}</ThemedText>
+    </ThemedView>
+  );
+};
