8a752204d6ff05c96928b0327d78895622eeb779
Matias Campos <matias98cs@gmail.com>
2024-11-18 13:14:08 -0300

    infite scroll - indicador de carga cuando se scrollea

--- app/infinite-scroll/index.tsx
+++ app/infinite-scroll/index.tsx
@@ -1,10 +1,12 @@
+import { useThemeColor } from "@/hooks/useThemeColor";
 import ThemedText from "@/presentation/shared/ThemedText";
 import ThemedView from "@/presentation/shared/ThemedView";
 import { useState } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ActivityIndicator } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 
 const InfiniteScrollScreen = () => {
+  const primaryColor = useThemeColor({}, "primary");
   const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
   const loadMore = () => {
@@ -24,6 +26,16 @@ const InfiniteScrollScreen = () => {
         renderItem={({ item }) => <ListItem number={item} />}
         onEndReached={loadMore}
         onEndReachedThreshold={0.6}
+        ListFooterComponent={() => (
+          <View
+            style={{
+              height: 150,
+              justifyContent: "center",
+            }}
+          >
+            <ActivityIndicator size={40} color={primaryColor} />
+          </View>
+        )}
       />
     </ThemedView>
   );
@@ -36,8 +48,8 @@ interface ListItemsProps {
 const ListItem = ({ number }: ListItemsProps) => {
   return (
     <Image
-      source={{ uri: `https://picsum.photos/id/${number}/500/400` }}
-      style={{ width: "100%", height: 400 }}
+      source={{ uri: `https://picsum.photos/id/${number}/500/350` }}
+      style={{ width: "100%", height: 350 }}
     />
   );
 };
